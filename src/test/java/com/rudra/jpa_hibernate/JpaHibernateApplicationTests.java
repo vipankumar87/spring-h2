2cc2645a506e7810380d9a4fbfc4212cdbb5ebd3
Vipan Kumar <vipan.webdeveloper@gmail.com>
2024-11-17 12:15:49 +0530

    init

---
+++ src/test/java/com/rudra/jpa_hibernate/JpaHibernateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rudra.jpa_hibernate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaHibernateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
